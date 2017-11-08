5656cec9ee338335fec62d13852d10a204b90b62
Ruud, Jan Egil <janegil.ruud@atmel.com>
2017-11-08 12:53:20 +0100

    Most things describing UPDI in place. Now I need a device description.

--- avrdude/libavrdude.h
+++ avrdude/libavrdude.h
@@ -199,6 +199,7 @@ typedef struct opcode {
 #define AVRPART_WRITE          0x0400  /* at least one write operation specified */
 #define AVRPART_HAS_TPI        0x0800  /* part has TPI i/f rather than ISP (ATtiny4/5/9/10) */
 #define AVRPART_IS_AT90S1200   0x1000  /* part is an AT90S1200 (needs special treatment) */
+#define AVRPART_HAS_UPDI       0x2000  /* part has UPDI i/f (AVR8X) */
 
 #define AVR_DESCLEN 64
 #define AVR_IDLEN   32
